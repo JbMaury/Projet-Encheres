245a2a651d6fb503ac1c2dfa4f762cc7fd05fca8
Samuel Bortolotti <sambortolotti@gmail.com>
2023-05-31 10:52:50 +0200

    Objets et classes ajoutées

--- src/main/java/fr/eni/projetencheres/bo/Encheres.java
+++ src/main/java/fr/eni/projetencheres/bo/Encheres.java
@@ -1,4 +1,13 @@
 package fr.eni.projetencheres.bo;
 
+import java.util.GregorianCalendar;
+
 public class Encheres {
+    private int dateEnchere extends GregorianCalendar;
+    private int montantEnchère;
+
+    public Encheres(int dateEnchere, int montantEnchère) {
+        this.dateEnchere = dateEnchere;
+        this.montantEnchère = montantEnchère;
+    }
 }
