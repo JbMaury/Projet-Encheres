e1c4e7f95a080faf111a34925323327eef05b737
Samuel Bortolotti <sambortolotti@gmail.com>
2023-05-31 11:14:24 +0200

    Objets et classes ajoutées bis

--- src/main/java/fr/eni/projetencheres/bo/Retrait.java
+++ src/main/java/fr/eni/projetencheres/bo/Retrait.java
@@ -1,4 +1,13 @@
 package fr.eni.projetencheres.bo;
 
 public class Retrait {
+    private String rue;
+    private int code_Postal;
+    private String ville;
+
+    public Retrait(String rue, int codePostal, String ville) {
+        this.rue = rue;
+        code_Postal = codePostal;
+        this.ville = ville;
+    }
 }
